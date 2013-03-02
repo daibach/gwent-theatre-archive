342defcf0cc37345f4c4d35c036965541a5c0c16
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2013-03-02 23:51:35 +0000

    add 410 page

---
+++ 410.php
@@ -0,0 +1,43 @@
+<?php
+header("HTTP/1.0 410 Gone");
+?>
+<!DOCTYPE
+ html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+  <meta name="keywords"    content="" />
+  <meta name="description" content="" />
+  <meta name="Author"      content="Dafydd Vaughan (2013), dafyddvaughan.co.uk" />
+  <meta name="robots"      content="all" />
+
+  <title>The page you have requested is no longer available | Theatre Gwent Theatre | 410 Gone</title>
+
+  <link rel="stylesheet" href="/css/blueprint-screen.css" type="text/css" media="screen, projection" />
+  <link rel="stylesheet" href="/css/blueprint-print.css" type="text/css" media="print" />
+  <link rel="stylesheet" href="/css/styles.css" type="text/css" media="screen, projection" />
+
+</head>
+<body>
+
+  <div id="head" class="home">
+    <div class="container">
+      <h1>theatr<br/>gwent<br/>theatre</h1>
+    </div>
+  </div>
+  <div class="container">
+    <div id="content">
+      <p><strong>Sorry, the Gwent Theatre Company closed in 2010</strong>. The page you were looking for is no longer here.</p>
+      <p>An archive of <strong>some</strong> of the old Gwent Theatre website is now online at <a href="http://www.gwenttheatre.com">gwenttheatre.com</a>.</p>
+      <p><strong>Gwent Young People's Theatre (GYPT)</strong> still operates - you can find out more on their <a href="https://twitter.com/gwentyptheatre">twitter account</a>.</p>
+    </div>
+
+    <div id="footer">
+      Archive maintained by <a href="http://www.dafyddvaughan.co.uk" title="Dafydd Vaughan">Dafydd Vaughan</a>
+    </div>
+  </div>
+
+</body>
+</html>
