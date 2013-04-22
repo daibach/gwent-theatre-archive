4130c3d94f66ff480049ea4140a514f5bf208f96
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2013-04-22 22:00:52 +0100

    reinstate google analytics code

--- 410.php
+++ 410.php
@@ -18,7 +18,19 @@ header("HTTP/1.0 410 Gone");
   <link rel="stylesheet" href="http://www.gwenttheatre.com/css/blueprint-screen.css" type="text/css" media="screen, projection" />
   <link rel="stylesheet" href="http://www.gwenttheatre.com/css/blueprint-print.css" type="text/css" media="print" />
   <link rel="stylesheet" href="http://www.gwenttheatre.com/css/styles.css" type="text/css" media="screen, projection" />
+  <script type="text/javascript">
 
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-3907434-3']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
 </head>
 <body>
 
