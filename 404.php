3bac960a006b1d12dad564fcc223950fd3470b88
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2014-06-28 19:54:58 +0100

    add 404 page

---
+++ 404.php
@@ -0,0 +1,66 @@
+<?php
+http_response_code(404);
+?>
+<!DOCTYPE
+ html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+ "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+
+  <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+  <meta name="keywords"    content="" />
+  <meta name="description" content="" />
+  <meta name="Author"      content="Dafydd Vaughan (2013), dafyddvaughan.co.uk" />
+  <meta name="robots"      content="all" />
+
+  <title>Page not found | Theatre Gwent Theatre | 404 Not found</title>
+
+  <link rel="stylesheet" href="http://www.gwenttheatre.com/css/blueprint-screen.css" type="text/css" media="screen, projection" />
+  <link rel="stylesheet" href="http://www.gwenttheatre.com/css/blueprint-print.css" type="text/css" media="print" />
+  <link rel="stylesheet" href="http://www.gwenttheatre.com/css/styles.css" type="text/css" media="screen, projection" />
+  <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-3907434-3']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
+</head>
+<body>
+
+  <div id="head" class="home">
+    <div class="container">
+      <h1>theatr<br/>gwent<br/>theatre <span>archive</span></h1>
+    </div>
+  </div>
+  <div id="menu">
+    <div class="container">
+      <ul>
+        <li><a href="http://www.gwenttheatre.com/">Introduction</a></li>
+        <li><a href="http://www.gwenttheatre.com/about.html">About Gwent Theatre</a></li>
+        <li><a href="http://www.gwenttheatre.com/galleries.html">Photo Galleries</a></li>
+        <li><a href="http://www.gwenttheatre.com/gypt.html">GYPT</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="container">
+    <div id="content">
+      <h2>This page is missing</h2>
+      <p>Sorry, the page you were looking for can't be found.</p>
+      <p>An archive of <strong>some</strong> of the old Gwent Theatre website is now online at <a href="http://www.gwenttheatre.com">gwenttheatre.com</a>.</p>
+      <p><strong>Gwent Young People's Theatre (GYPT)</strong> still operates - you can find out more on their <a href="https://twitter.com/gwentyptheatre">twitter account</a>.</p>
+    </div>
+
+    <div id="footer">
+      <a href="https://github.com/daibach/gwent-theatre-archive">Archive</a> maintained by <a href="http://www.dafyddvaughan.co.uk" title="Dafydd Vaughan">Dafydd Vaughan</a>
+    </div>
+  </div>
+
+</body>
+</html>
